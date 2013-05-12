019ccad6f10b634d799d8bd7e33c69a4993b1284
Lukas Rytz <lukas.rytz@epfl.ch>
2013-05-12 12:06:47 +0200

    custom theme

--- Gruntfile.js
+++ Gruntfile.js
@@ -46,6 +46,7 @@ module.exports = function(grunt) {
 					'css/theme/night.css': 'css/theme/source/night.scss',
 					'css/theme/serif.css': 'css/theme/source/serif.scss',
 					'css/theme/simple.css': 'css/theme/source/simple.scss',
+					'css/theme/simple-luc.css': 'css/theme/source/simple-luc.scss',
 					'css/theme/sky.css': 'css/theme/source/sky.scss',
 					'css/theme/moon.css': 'css/theme/source/moon.scss',
 					'css/theme/solarized.css': 'css/theme/source/solarized.scss'
